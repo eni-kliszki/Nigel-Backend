91c19bfd4c4020b76fea2399f142cd3cac8c6916
eni-kliszki <kliszkieniko@gmail.com>
2021-02-09 10:28:56 +0100

    add service-registry project

---
+++ service-registry/src/test/java/com/nigel/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nigel.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
