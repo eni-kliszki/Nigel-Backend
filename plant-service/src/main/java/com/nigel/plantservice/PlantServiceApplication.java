5d14cc9e080350daf3b465dbbf1e080e695db051
eni-kliszki <kliszkieniko@gmail.com>
2021-02-09 10:42:48 +0100

    add application properties

--- plant-service/src/main/java/com/nigel/plantservice/PlantServiceApplication.java
+++ plant-service/src/main/java/com/nigel/plantservice/PlantServiceApplication.java
@@ -2,8 +2,10 @@ package com.nigel.plantservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PlantServiceApplication {
 
     public static void main(String[] args) {
